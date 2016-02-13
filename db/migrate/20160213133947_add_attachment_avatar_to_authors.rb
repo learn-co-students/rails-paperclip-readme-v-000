8ee479efc424aafc3a2b43e79e8ce1a62e168c6f
Lukeghenco <lukeghenco@gmail.com>
2016-02-13 09:45:17 -0500

    Done.

---
+++ db/migrate/20160213133947_add_attachment_avatar_to_authors.rb
@@ -0,0 +1,9 @@
+class AddAttachmentAvatarToAuthors < ActiveRecord::Migration
+  def self.up
+    add_attachment :authors, :avatar
+  end
+
+  def self.down
+    remove_attachment :authors, :avatar
+  end
+end
