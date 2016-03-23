515d7ed00e2657eb5e788ea7d1e9335f618626c3
pajamaw <pjpeterww@gmail.com>
2016-03-23 14:35:43 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,12 @@
 class Author < ActiveRecord::Base
   has_many :posts
+  has_attached_file :avatar, default_url: ':style/default.png',
+   styles: { thumb: "100x100>" }
+
+  #We have to place an image named default.png in our
+  #app\assets\images\thumb and app\assets\images\original folders 
+  #(already provided), that part doesn't 
+  #automatically happen via Paperclip (Image)Magick.
+  validates_attachment_content_type :avatar, 
+  content_type: /\Aimage\/.*\Z/
 end
