be1c7117965e5133ec726902d8f55d26d579bcd7
davdkm <davd.km@gmail.com>
2016-06-13 21:30:21 -0700

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -29,6 +29,6 @@ class AuthorsController < ApplicationController
   private
 
   def author_params
-    params.require(:author).permit(:bio, :name)
+    params.require(:author).permit(:bio, :name, :avatar)
   end
 end
