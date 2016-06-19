839476679b27e617fa55ecfda36d4563765fcb13
gormanjp <jpgorman8@gmail.com>
2016-06-19 11:50:04 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,12 @@
 class Author < ActiveRecord::Base
   has_many :posts
+   has_attached_file :avatar, default_url: ':style/default.png', styles: { thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+    private
+ 
+  def author_params
+    params.require(:author).permit(:bio, :name, :avatar)
+  end
+
 end
