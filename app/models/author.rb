b96c0561a1bd71c0889dbf4ce16ae215e95fc6a0
James Fernandez <jffernan@comcast.net>
2017-08-28 15:20:32 -0700

    Update author.rb

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
   has_many :posts
-  has_attached_file :avatar
+  has_attached_file :avatar, default_url: ':style/default.png'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
