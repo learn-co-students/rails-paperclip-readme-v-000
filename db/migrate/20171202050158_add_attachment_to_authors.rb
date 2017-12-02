3338a6f91a21bd51ad802e2d10623f0b2edfcefc
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-12-02 05:05:43 +0000

    Done.

---
+++ db/migrate/20171202050158_add_attachment_to_authors.rb
@@ -0,0 +1,10 @@
+class AddAttachmentToAuthors < ActiveRecord::Migration
+  def self.up
+    change_table :authors do |t|
+      t.attachment :avatar
+    end
+  end
+  def self.down
+    remove_attachment :authors, :avatar
+  end
+end
