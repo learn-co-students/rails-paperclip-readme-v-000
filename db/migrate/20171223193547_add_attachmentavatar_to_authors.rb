f1befe0bf3a34ad6f6893c06e683155419e73755
jamesonbass1987 <jamesonbass@gmail.com>
2017-12-23 11:46:09 -0800

    Done.

---
+++ db/migrate/20171223193547_add_attachmentavatar_to_authors.rb
@@ -0,0 +1,12 @@
+class AddAttachmentavatarToAuthors < ActiveRecord::Migration
+  def self.up
+      change_table :authors do |t|
+        t.attachment :avatar
+      end
+  end
+
+  def self.down
+      remove_attachment :authors, :avatar
+  end
+
+end
